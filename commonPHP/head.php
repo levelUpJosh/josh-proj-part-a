ede899b582d5c820cc92f255bf07ad914943725e
Josh <levelUpJosh@users.noreply.github.com>
2021-10-20 20:15:34 +0100

    updated commonPHP

---
+++ commonPHP/head.php
@@ -0,0 +1,8 @@
+<!-- the head tags are defined in the outer file -->
+
+<!-- Title template -->
+<title> <?php echo $pageTitle ?> | Make-It-All Helpdesk </title>
+
+<!-- Bootstrap CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>

a66781b3000011d6096ab8f3a34c1bee11547741
Josh <levelUpJosh@users.noreply.github.com>
2021-10-20 20:15:49 +0100

    Create template.php

---
+++ template.php
@@ -0,0 +1,12 @@
+<!-- This file merely contains the template for starting a new page. 
+It imports the head and footer php from commonPHP-->
+
+<head>
+    <?php 
+    $pageTitle = "";
+    include "commonPHP/head.php";
+    ?>
+</head>
+<body>
+    <?php include "commonPHP/footer.php"?>
+</body>

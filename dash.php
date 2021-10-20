f3405bd58d0838cff13f38f05895c831160ff744
Josh <levelUpJosh@users.noreply.github.com>
2021-10-20 22:56:07 +0100

    Update dash.php
    Laid foundation for general layout

--- dash.php
+++ dash.php
@@ -5,24 +5,48 @@
     ?>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row d-flex">
-            <nav id="sideMenu" class="col-md-4 col-lg-3 d-md-block bg-light  sidebar collapse">
-                <div class="position-sticky">
+    <div class="container-fluid vh-100 d-flex flex-column">
+        <div class="row bg-dark text-light text-center">
+            <h1>Make-It-All Helpdesk </h1>
+        </div>
+        <div class="row flex-grow-1">
+        <!-- <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#sideMenu" aria-expanded="false" aria-controls="sideMenu">
+                Button that hides the sidebar
+            </button> -->
+            <nav id="sideMenu" class="col-md-4 col-lg-2 bg-light sidebar border collapse-horizontal">
+            
+                <div class="position-sticky w-auto">
                     <ul class="nav flex-column">
-                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Lookup</h6>
-                        <li class="nav-item">Employee Lookup</li>
-                        <li class="nav-item">Hardware Lookup</li>
-                        <li class="nav-item">Software Lookup</li>
-                        <li class="nav-item">Specialist Lookup</li>
+                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Lookup</h4>
+                        <li class="nav-item btn btn-outline-primary mb-1">Employee Lookup</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Hardware Lookup</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Software Lookup</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Specialist Lookup</li>
 
-                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Logs</h6>
-                        <li class="nav-item">Problem Log</li>
-                        <li class="nav-item">Call Log</li>
+                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Logs</h4>
+                        <li class="nav-item btn btn-outline-primary mb-1">Problem Log</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Call Log</li>
                     </ul>
                 </div>
 
             </nav>
+            <div class="container-fluid flex-fill  col-md-8" id="dashContent">
+    hello
+            </div>
+            <!-- <ul class="nav flex-column border">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul> -->
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

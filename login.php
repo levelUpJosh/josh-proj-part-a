b4e09b619376e07e6c1d6af46b94f10264f53ac3
Josh <levelUpJosh@users.noreply.github.com>
2021-10-25 18:31:06 +0100

    Added Specialist Log in

--- login.php
+++ login.php
@@ -47,14 +47,7 @@
                 $('#loginAlertBox').removeClass("d-none alert-danger");
                 window.location.href = "operator-dash.php";
                 console.log("Success");
-            } else {
-                console.log("Error");
-                $('#loginAlertBox').text('Incorrect username or password entered.');
-                $('#loginAlertBox').addClass("alert-danger");
-                $('#loginAlertBox').removeClass("d-none alert-primary");
-            }
-
-            if (username == "Specialist" && password == "SpecialistPass") {
+            } else if (username == "Specialist" && password == "SpecialistPass") {
                 $('#loginAlertBox').text('Successful login');
                 $('#loginAlertBox').addClass("alert-primary");
                 $('#loginAlertBox').removeClass("d-none alert-danger");

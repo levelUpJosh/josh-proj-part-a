ebc815e4037182701e0a3eba42944328febb7872
Josh <levelUpJosh@users.noreply.github.com>
2021-11-01 13:50:36 +0000

    Update test.php
    Added table search function with comments

--- dash-content/test.php
+++ dash-content/test.php
@@ -1,6 +1,13 @@
-<input type="text" id="emp-search" onkeyup="searchTable()" placeholder="Search">
 
-<table class="table table-striped" id="emp-table">
+<div class="position-sticky fixed-top row bg-light">
+    <div>
+        <label for="emp-search">Enter Search Query: </label>
+        <input type="text" id="emp-search" onkeyup="searchTable()" placeholder="Search">
+        <button type="button" class="btn btn-primary col-2 float-right m-3">Add New Employee</button>
+    </div>
+    
+</div>
+<table class="table" id="emp-table">
     <thead>
         <th scope="col">Employee ID</th>
         <th scope="col">First Name</th>
@@ -715,32 +722,68 @@
 
 <script>
     function searchTable(){
-        var input,search ,table,tr,td,i,txt;
+        // Allows text to be entered in the search box to find in the table.
+        // Does not involve searching the database again therefore reduces security risk of SQL injection.
+
+        // Set the variables
+        var input,search ,table,tr,td,i,txt,count;
+
+        // Find the input text field
         input = document.getElementById("emp-search");
-        console.log(input);
+        //console.log(input);
+        // Retrieve the search from the field
         search = input.value.toUpperCase();
+
+        // Find the table and all table rows
         table = document.getElementById("emp-table");
         tr = table.getElementsByTagName("tr");
 
+        // Reset the count of rows to zero
+        // This is used to recolour the new searched table for readability purposes
+        count = 0;
+
+        // for loop through all rows
         for (i = 0; i < tr.length; i++) {
+            // Get all table data (elements/cells) 
             all_td = tr[i].getElementsByTagName("td");
             console.log(all_td);
+
+            // for loop through all cells in the row
             for (j = 0; j < all_td.length; j++) {
+
+                // get the current cell
                 td = all_td[j];
                 console.log(td.textContent);
+
+                // if a cell exists
                 if (td) {
+                    
+                    // Get the text content of the cell
                     txt = td.textContent || td.innerText;
                     console.log(txt);
+
+                    // if the uppercase cell contains the uppercase search then
                     if (txt.toUpperCase().indexOf(search) > -1) {
-                        tr[i].style.display = "";
-                        console.log("found");
+                        // add 1 to the count of found rows
+                        count ++; 
+
+                        // use count to stripe the table for readability
+                        if (count % 2 == 1) {
+                            tr[i].classList.toggle("table-light",true) // true here means only add the class, don't remove it
+                        } else {
+                            tr[i].classList.toggle("table-light",false) // where false here means only remove the class.
+                        }
+                        tr[i].classList.remove("d-none"); // remove the class hiding the row
+                        // exit the loop to avoid checking any further cells from this row
                         break;
                     } else {
-                        tr[i].style.display = "none";
+
+                        // Hide rows that did not contain the search
+                        tr[i].classList.add("d-none");
                     }
                 }
             }
         }
-
     }
+    searchTable(); //run this so that the table stripes properly
 </script>

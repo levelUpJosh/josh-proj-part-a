7f2656ba08281c5d0ca99d66230679d3e460e762
Josh <levelUpJosh@users.noreply.github.com>
2021-11-01 13:51:48 +0000

    Update operator-dash.php
    adjusted operator dashboard

--- operator-dash.php
+++ operator-dash.php
@@ -16,23 +16,27 @@
             </button> -->
             <nav id="sideMenu" class="col-md-4 col-lg-2 bg-light sidebar border collapse-horizontal">
             
-                <div class="position-sticky w-auto">
+                <div class="position-sticky fixed-top w-auto">
                     <ul class="nav flex-column">
-                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Lookup</h4>
-                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Employee Lookup</li>
-                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Hardware Lookup</li>
-                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Software Lookup</li>
-                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Specialist Lookup</li>
+                        <!-- Need to work out how best to navigate between panels which are currently accessed through ?content= in the URL -->
+                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Databases</h4>
+                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Employee</li>
+                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Hardware</li>
+                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Software</li>
+                        <li onclick="location.reload()" class="nav-item btn btn-outline-primary mb-1">Specialist</li>
 
                         <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Logs</h4>
-                        <li class="nav-item btn btn-outline-primary mb-1">Problem Log</li>
-                        <li class="nav-item btn btn-outline-primary mb-1">Call Log</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Problems</li>
+                        <li class="nav-item btn btn-outline-primary mb-1">Calls</li>
+                        <br>
+                        <button type="button" class="btn btn-primary d-none d-md-inline-block" id="topBtn" onclick="topFunction()">Return to Top</button>
                     </ul>
                 </div>
 
             </nav>
-            <div class="container-fluid flex-fill  col-md-8" id="dashContent">
+            <div class="container-fluid flex-fill overflo-auto m-1 col-md-7" id="dashContent">
                 <?php 
+                # import the selected panel
                 include "dash-content/".$dashContent.".php";
                 isset($GET["content"]) ? include "dash-content/".$dashContent.".php" : "did not find content"; # is content variable set? if so include that panel.
                 ?>
@@ -56,5 +60,27 @@
   </button>
         </div>
     </div>
+    <script>
+        //Get the button:
+        mybutton = $("#topbtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.fadeIn();
+        } else {
+            mybutton.fadeOut();
+        }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+        
+    </script>
     <?php include "commonPHP/footer.php"?>
 </body>

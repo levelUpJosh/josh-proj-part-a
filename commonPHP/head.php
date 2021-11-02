1b7da065510abeeb1136bcfefe18898ed353be6f
Josh <levelUpJosh@users.noreply.github.com>
2021-11-02 02:10:01 +0000

    Update head.php
    added url function

--- commonPHP/head.php
+++ commonPHP/head.php
@@ -3,6 +3,10 @@
 <!-- Title template -->
 <title> <?php echo $pageTitle ?> | Make-It-All Helpdesk </title>
 
+<?php #get current url and make it accessible to the code
+$pageURL = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+?>
+
 <!-- Bootstrap CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>

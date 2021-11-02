710b5b8dbff53cf75d035c4b947a552da64ceb86
Josh <levelUpJosh@users.noreply.github.com>
2021-11-02 22:55:17 +0000

    Created user setting pages
    Created change password page and backups page

---
+++ dash-content/user-settings/backups.php
@@ -0,0 +1,13 @@
+<form class="card">
+    <div class="card-body">
+        <div class="mb-3 row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Backups Enabled:</label>
+            <div class="col-sm-10">
+                <input type="checkbox" readonly class="form-control-plaintext" id="backupCheckbox" checked>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+    </div>
+</form>
---
+++ dash-content/user-settings/change-password.php
@@ -0,0 +1,26 @@
+<form class="card">
+    <div class="card-body">
+        <div class="mb-3 row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Username</label>
+            <div class="col-sm-10">
+                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="Alice">
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword">
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="confirmPassword" class="col-sm-2 col-form-label">Confirm Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="confirmPassword">
+            </div>
+
+        </div>
+        <div class="mb-3 row">
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </div>
+    </div>
+</form>

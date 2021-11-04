23e71143bc746b75e1eac23d5460a4179bf8e543
Josh <levelUpJosh@users.noreply.github.com>
2021-11-04 19:04:38 +0000

    Update hardware.php

--- dash-content/databases/hardware.php
+++ dash-content/databases/hardware.php
@@ -1,4 +1,4 @@
-<!-- Modal -->
+<!-- Edit Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">

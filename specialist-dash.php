17167bcd81263118414ef12279fb045b7f5435da
Josh <levelUpJosh@users.noreply.github.com>
2021-11-06 16:28:02 +0000

    Update dash containers
    Updated both dashboard containers to support the new links and GET options (return_url, show_modal and show_add_modal). These may become POST in the future but for now they are obvious GET variables

--- specialist-dash.php
+++ specialist-dash.php
@@ -2,15 +2,24 @@
     <?php
     $pageTitle = "Specialist Dashboard";
     $userType = "specialist";
-    
-    if (isset($_GET["content"])){
+
+    if (isset($_GET["content"])) {
         $dashContent = htmlspecialchars($_GET["content"]); # use this to set what section should be shown
-    }else{
+    } else {
         #default to problems page
         $dashContent = "logs/problems";
     }
-    #echo $dashContent;
+    echo $dashContent;
     include "commonPHP/head.php";
+
+    function checkReturnUrl($userType) {
+        if (isset($_GET["return_url"])) {
+            $return_url = $_GET["return_url"];
+            echo '<button type="button" class="btn btn-secondary" onclick="location.replace(\''.$userType.'-dash?content='.$return_url.'&show_modal=Y\');">Return to last page</button>';
+        } else {
+            echo '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>';
+            }
+    }
     ?>
 </head>
 
@@ -25,34 +34,34 @@
             </button> -->
             <nav id="sideMenu" class="col-md-4 col-lg-2 bg-light sidebar border collapse-horizontal">
 
-                <div class="position-sticky fixed-top w-auto">
-                    <ul class="nav flex-column">
-                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Logs</h4>
-                        <li class="nav-item btn btn-outline-primary mb-1 <?php if ($dashContent == 'logs/problems') {
-                                                                                echo 'disabled';
-                                                                            }; ?>" onclick="location.replace('/<?php echo $userType ?>-dash?content=logs/problems');">Problems</li>
-                        <li class="nav-item btn btn-outline-primary mb-1 <?php if ($dashContent == 'logs/calls') {
-                                                                                echo 'disabled';
-                                                                            }; ?>" onclick="location.replace('/<?php echo $userType ?>-dash?content=logs/calls');">Calls</li>
+                <div class="position-sticky fixed-top">
+                    <ul class="nav flex-column ">
+                        <!-- Need to work out how best to navigate between panels which are currently accessed through ?content= in the URL -->
+                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted w-100">Logs</h4>
+                        <li class="nav-item btn btn-outline-primary w-100 mb-1 <?php if ($dashContent == 'logs/problems') {
+                                                                                    echo 'disabled';
+                                                                                }; ?>" onclick="location.replace('/<?php echo $userType ?>-dash?content=logs/problems');">Problems</li>
+                        <li class="nav-item btn btn-outline-primary w-100 mb-1 <?php if ($dashContent == 'logs/calls') {
+                                                                                    echo 'disabled';
+                                                                                }; ?>" onclick="location.replace('/<?php echo $userType ?>-dash?content=logs/calls');">Calls</li>
 
-                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted">Databases</h4>
-                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/emp');" class="nav-item btn btn-outline-primary mb-1 <?php if ($dashContent == 'databases/emp') {
-                                                                                                                                                    echo 'disabled';
-                                                                                                                                                }; ?>">Employee</li>
-                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/hardware');" class="nav-item btn btn-outline-primary mb-1 <?php if ($dashContent == 'databases/hardware') {
-                                                                                                                                                        echo 'disabled';
-                                                                                                                                                    }; ?>">Hardware</li>
-                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/software');" class="nav-item btn btn-outline-primary mb-1 <?php if ($dashContent == 'databases/software') {
-                                                                                                                                                        echo 'disabled';
-                                                                                                                                                    }; ?>">Software</li>
+                        <h4 class="sidebar-heading text-center align-items-center px-3 mt-3 mb-3 text-muted w-100">Databases</h4>
+                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/emp');" class="nav-item btn btn-outline-primary mb-1 w-100 <?php if ($dashContent == 'databases/emp') {
+                                                                                                                                                                        echo 'disabled';
+                                                                                                                                                                    }; ?>">Employee</li>
+                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/hardware');" class="nav-item btn btn-outline-primary mb-1 w-100 <?php if ($dashContent == 'databases/hardware') {
+                                                                                                                                                                            echo 'disabled';
+                                                                                                                                                                        }; ?>">Hardware</li>
+                        <li onclick="location.replace('/<?php echo $userType ?>-dash?content=databases/software');" class="nav-item btn btn-outline-primary mb-1 w-100 <?php if ($dashContent == 'databases/software') {
+                                                                                                                                                                            echo 'disabled';
+                                                                                                                                                                        }; ?>">Software</li>
                         <br>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <div class="btn btn-outline-primary col-12">Account Settings</div>
+                        <li class="nav-item dropdown w-100">
+                            <a class="btn btn-outline-primary nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <div class="  col-12">Account Settings</div>
                             </a>
                             <ul class="dropdown-menu col-12" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" onclick="location.replace('/<?php echo $userType ?>-dash?content=user-settings/change-password');">Change Password</a></li>
-                                <li>
+                                <li><a class="dropdown-item" onclick="location.replace('/<?php echo $userType ?>-dash?content=user-settings/change-password');">Change Password</a></li>                                <li>
                                     <hr class="dropdown-divider">
                                 </li>
                                 <li><a class="dropdown-item" href="/login.php">Log Out</a></li>
@@ -67,12 +76,9 @@
             </nav>
             <div class="container-fluid flex-fill overflow-auto m-1 col-md-7" id="dashContent">
                 <?php
-                if (file_exists("dash-content/" . $dashContent . ".php")) {
-                    # import the selected panel
-                    include "dash-content/" . $dashContent . ".php";
-                } else {
-                    echo "content panel '".$dashContent."' not found";
-                }
+                # import the selected panel
+                include "dash-content/" . $dashContent . ".php";
+                isset($GET["content"]) ? include "dash-content/" . $dashContent . ".php" : "did not find content"; # is content variable set? if so include that panel.
                 ?>
             </div>
             <!-- <ul class="nav flex-column border">
@@ -94,28 +100,43 @@
             </button>
         </div>
     </div>
-    <script>
-        //Get the button:
-        mybutton = $("#topbtn");
-
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {
-            scrollFunction()
-        };
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.fadeIn();
-            } else {
-                mybutton.fadeOut();
-            }
-        }
-
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            document.body.scrollTop = 0; // For Safari
-            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        }
-    </script>
     <?php include "commonPHP/footer.php" ?>
+    <script>
+        $(window).load(function() {
+            //Get the button:
+            mybutton = $("#topbtn");
+
+            // When the user scrolls down 20px from the top of the document, show the button
+            window.onscroll = function() {
+                scrollFunction()
+            };
+
+            function scrollFunction() {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                    mybutton.fadeIn();
+                } else {
+                    mybutton.fadeOut();
+                }
+            }
+
+            // When the user clicks on the button, scroll to the top of the document
+            function topFunction() {
+                document.body.scrollTop = 0; // For Safari
+                document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+            }
+            <?php if (isset($_GET['show_modal'])) {
+                if ($_GET['show_modal'] == 'Y') {
+                    # if the get variable to show modal is set then show. Temporary to show how this would work but change to JS later
+                    echo "$('#".$panel."EditModal').modal('show');";
+                }
+            } 
+            if (isset($_GET['show_add_modal'])) {
+                if ($_GET['show_add_modal'] == 'Y') {
+                    # if the get variable to show modal is set then show. Temporary to show how this would work but change to JS later
+                    echo "$('#".$panel."AddModal').modal('show');";
+                }
+                }?>
+        })
+    </script>
+    
 </body>
